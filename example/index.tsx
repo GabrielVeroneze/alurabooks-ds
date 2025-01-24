4889ae880586ab1c31c3041c668b9813404190a6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-23 23:25:12 -0300

    ♻️ refactor: substitui componente Thing pelo componente AbBotao

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,14 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { AbBotao } from '../src';
 
 const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
+    return (
+        <div>
+            <AbBotao />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
