922231ffa03254281c8681b21eb2e815145a863d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-23 22:14:12 -0300

    💄 feat: estiliza o componente AbBotao utilizando styled-components

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const BotaoEstilizado = styled.button`
+    background: #EB9B00;
+    border: 2px solid #EB9B00;
+    color: #FFFFFF;
+    cursor: pointer;
+    font-size: 20px;
+    padding: 16px 32px;
+
+    &:hover {
+      background: #B87900;
+      border: 2px solid #B87900;
+    }
+`
 
 const AbBotao = () => {
-  return <button>Clique aqui!</button>;
+  return <BotaoEstilizado>Clique aqui!</BotaoEstilizado>;
 };
 
 export default AbBotao;
