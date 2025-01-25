93d6fce80b8af9c343fb924e73540b91cd058658
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-24 23:28:51 -0300

    ♻️ refactor: separa estilização do componente AbBotao

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,19 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const BotaoEstilizado = styled.button`
-    background: #EB9B00;
-    border: 2px solid #EB9B00;
-    color: #FFFFFF;
-    cursor: pointer;
-    font-size: 20px;
-    padding: 16px 32px;
-
-    &:hover {
-      background: #B87900;
-      border: 2px solid #B87900;
-    }
-`
 
 export const AbBotao = () => {
     return (
---
+++ src/components/AbBotao/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Botao = styled.button`
+    background: #eb9b00;
+    border: 2px solid #eb9b00;
+    color: #ffffff;
+    cursor: pointer;
+    font-size: 20px;
+    padding: 16px 32px;
+
+    &:hover {
+        background: #b87900;
+        border: 2px solid #b87900;
+    }
+`
