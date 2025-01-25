47d8c5a5f93d1840d75108d9fb5881321f5cf561
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-24 23:30:18 -0300

    ♻️ refactor: atualiza importação e uso do componente estilizado no AbBotao

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from 'react'
+import { Botao } from './styled'
 
 export const AbBotao = () => {
     return (
-        <BotaoEstilizado>Clique aqui!</BotaoEstilizado>
+        <Botao>Clique aqui!</Botao>
     )
-};
+}
