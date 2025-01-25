fa5b191a0786d06b2f2193ba1cf1f98434f08ff8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-25 17:17:09 -0300

    ✨ feat: adiciona suporte a propriedades `texto`, `tipo` e `onClick`

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Botao } from './styled'
 
-export const AbBotao = () => {
+export interface AbBotaoProps {
+    texto?: string
+    tipo?: 'primario' | 'secundario'
+    onClick?: () => void
+}
+
+export const AbBotao = ({ texto, tipo = 'primario', onClick }: AbBotaoProps) => {
     return (
-        <Botao>Clique aqui!</Botao>
+        <Botao tipo={tipo} onClick={onClick}>
+            {texto}
+        </Botao>
     )
 }
