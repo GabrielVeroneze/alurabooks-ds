6d6d60ac59f857af071f0420de308d24d7189646
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-25 17:34:27 -0300

    💄 feat: adiciona estilos dinâmicos ao componente Botao com base no tipo

--- src/components/AbBotao/styled.ts
+++ src/components/AbBotao/styled.ts
@@ -1,15 +1,27 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { AbBotaoProps } from './'
 
-export const Botao = styled.button`
-    background: #eb9b00;
+export const Botao = styled.button<AbBotaoProps>`
+    background: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#eb9b00' : '#ffffff'};
     border: 2px solid #eb9b00;
-    color: #ffffff;
+    color: ${(props: AbBotaoProps) => props.tipo === 'primario' ? '#ffffff' : '#eb9b00'};
     cursor: pointer;
     font-size: 20px;
     padding: 16px 32px;
 
-    &:hover {
-        background: #b87900;
-        border: 2px solid #b87900;
-    }
+    ${(props: AbBotaoProps) => props.tipo === 'primario'
+        ? css`
+            &:hover {
+                background: #b87900;
+                border: 2px solid #b87900;
+            }
+        `
+        : css`
+            &:hover {
+                color: #b87900;
+                background: #ffffff;
+                border: 2px solid #b87900;
+            }
+        `
+    };
 `
