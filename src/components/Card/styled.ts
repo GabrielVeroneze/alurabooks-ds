243c70f162cb6f4bf2d47287da9c02ddcd8f5fe8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-24 23:41:34 -0300

    💄 feat: cria estilo para o componente Card

---
+++ src/components/Card/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const CardWrapper = styled.div`
+    background: #ffffff;
+    border-radius: 10px;
+    box-shadow: 0px 4px 4px 0px #00000040;
+    padding: 48px;
+`
