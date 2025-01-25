475123f66d1e4c78e0a32f24c70f04bcfe93cc7d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-24 23:31:20 -0300

    🐛 fix: ajusta importação do componente AbBotao no Storybook

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import AbBotao from '../src/components/AbBotao';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react'
+import { AbBotao } from '../src/components/AbBotao'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
-  title: 'Componentes/AbBotao',
-  component: AbBotao,
-} as ComponentMeta<typeof AbBotao>;
+    title: 'Componentes/AbBotao',
+    component: AbBotao
+} as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />;
+const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
 
-export const Primario = Template.bind({});
+export const Primario = Template.bind({})
