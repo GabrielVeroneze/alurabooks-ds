cee3fb0b9fd3f982ef7f3f5683bb3f168eb6b331
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-24 23:46:56 -0300

    ✨ feat: adiciona componente Card no exemplo

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,17 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { AbBotao } from '../src';
+import 'react-app-polyfill/ie11'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { AbBotao, Card } from '../src'
 
 const App = () => {
     return (
         <div>
             <AbBotao />
+            <Card>
+                <h1>Olá, eu sou um card</h1>
+            </Card>
         </div>
-    );
-};
+    )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
