a14bc2ed3dcedc2510dfe71cc875e4f9282485e0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-25 23:27:42 -0300

    ✨ feat: adiciona estilos para o componente AbTag

---
+++ src/components/AbTag/styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const TagWrapper = styled.div`
+    background: #eb9b00;
+    color: #ffffff;
+    display: inline-block;
+    font-family: sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    padding: 24px 32px;
+`
