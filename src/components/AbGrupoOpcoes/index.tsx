b09b9119d084b1f18cbace40b428a31888cbb837
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-26 18:22:59 -0300

    ♻️ refactor: simplifica componentes internos no AbGrupoOpcoes

--- src/components/AbGrupoOpcoes/index.tsx
+++ src/components/AbGrupoOpcoes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Formatos, Header, Opcao, Preco } from './styled'
+import { Opcao } from './styled'
 
 export interface AbGrupoOpcoesProps {}
 
@@ -7,25 +7,19 @@ export const AbGrupoOpcoes = () => {
     return (
         <>
             <Opcao selecionado={false}>
-                <Header selecionado={false}>E-book</Header>
-                <Preco selecionado={false}>
-                    <strong>R$ 00,00</strong>
-                </Preco>
-                <Formatos selecionado={false}>.pdf, .epub, .mob</Formatos>
+                <header>E-book</header>
+                <div><strong>R$ 00,00</strong></div>
+                <footer>.pdf, .epub, .mob</footer>
             </Opcao>
             <Opcao selecionado={true}>
-                <Header selecionado={true}>E-book</Header>
-                <Preco selecionado={true}>
-                    <strong>R$ 00,00</strong>
-                </Preco>
-                <Formatos selecionado={true}>.pdf, .epub, .mob</Formatos>
+                <header>E-book</header>
+                <div><strong>R$ 00,00</strong></div>
+                <footer>.pdf, .epub, .mob</footer>
             </Opcao>
             <Opcao selecionado={false}>
-                <Header selecionado={false}>E-book</Header>
-                <Preco selecionado={false}>
-                    <strong>R$ 00,00</strong>
-                </Preco>
-                <Formatos selecionado={false}>.pdf, .epub, .mob</Formatos>
+                <header>E-book</header>
+                <div><strong>R$ 00,00</strong></div>
+                <footer>.pdf, .epub, .mob</footer>
             </Opcao>
         </>
     )
