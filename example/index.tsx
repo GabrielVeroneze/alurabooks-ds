122798925cae4c4f857ab58ec65009f7b26874b4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-26 23:56:27 -0300

    ✨ feat: adiciona AbGrupoOpcoes ao App de exemplo

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { AbBotao, AbCard, AbTag } from '../src'
+import { AbBotao, AbCard, AbGrupoOpcoes, AbTag } from '../src'
 
 const App = () => {
     return (
@@ -11,6 +11,28 @@ const App = () => {
                 <h1>Olá, eu sou um card</h1>
             </AbCard>
             <AbTag texto="Tag" />
+            <AbGrupoOpcoes
+                opcoes={[
+                    {
+                        id: 1,
+                        titulo: 'E-book',
+                        corpo: 'R$ 00,00',
+                        rodape: '.pdf, .epub, .mob'
+                    },
+                    {
+                        id: 2,
+                        titulo: 'Impresso',
+                        corpo: 'R$ 00,00',
+                        rodape: '.pdf, .epub, .mob'
+                    },
+                    {
+                        id: 3,
+                        titulo: 'Impresso + E-book',
+                        corpo: 'R$ 00,00',
+                        rodape: '.pdf, .epub, .mob'
+                    }
+                ]}
+            />
         </div>
     )
 }
