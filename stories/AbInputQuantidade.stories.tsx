93a9a060244f21244fdf14bb9790b012789e71f8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-29 23:45:26 -0300

    ✨ feat: adiciona story para AbInputQuantidade

---
+++ stories/AbInputQuantidade.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { AbInputQuantidade, AbInputQuantidadeProps } from '../src'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+export default {
+    title: 'Componentes/AbInputQuantidade',
+    component: AbInputQuantidade,
+    parameters: { actions: { argTypesRegex: '^on.*' } }
+} as ComponentMeta<typeof AbInputQuantidade>
+
+const Template: ComponentStory<typeof AbInputQuantidade> = args => <AbInputQuantidade {...args} />
+
+export const AbInputQuantidadeComponent = Template.bind({})
+
+AbInputQuantidadeComponent.args = {} as AbInputQuantidadeProps
