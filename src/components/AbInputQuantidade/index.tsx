39a02b231be60c61a0ba533856ad97b8bd017fe1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-01-29 22:59:46 -0300

    ✨ feat: adiciona controle de quantidade em AbInputQuantidade

--- src/components/AbInputQuantidade/index.tsx
+++ src/components/AbInputQuantidade/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { Input } from './styled'
+import React, { useEffect, useState } from 'react'
+import { Botao, Span, ControlesWrapper, Wrapper, Label } from './styled'
 
-const AbInputQuantidade = () => {
-    return (
-        <Input>AbInputQuantidade</Input>
-    )
+export interface AbInputQuantidadeProps {
+    onChange?: (value: number) => void
 }
 
-export default AbInputQuantidade
+export const AbInputQuantidade = ({ onChange }: AbInputQuantidadeProps) => {
+    const [value, setValue] = useState<number>(1)
+
+    useEffect(() => {
+        if (onChange) {
+            onChange(value)
+        }
+    }, [value])
+
+    return (
+        <Wrapper>
+            <Label>Quantidade</Label>
+            <ControlesWrapper>
+                <Botao onClick={() => setValue(prev => prev - 1)}>-</Botao>
+                <Span>{String(value).padStart(2, '0')}</Span>
+                <Botao onClick={() => setValue(prev => prev + 1)}>+</Botao>
+            </ControlesWrapper>
+        </Wrapper>
+    )
+}
